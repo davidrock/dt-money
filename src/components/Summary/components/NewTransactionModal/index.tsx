a70ad9992f446ba1ab2b79ab007a6d2fe7c0ae93
David Rocha <david.martinelli@farfetch.com>
2023-04-23 23:42:41 +0100

    create transaction type radiobuttons

--- src/components/Summary/components/NewTransactionModal/index.tsx
+++ src/components/Summary/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
-import { X } from "phosphor-react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
@@ -17,6 +23,16 @@ export function NewTransactionModal() {
             <input type="text" placeholder="Description" required />
             <input type="number" placeholder="Price" required />
             <input type="text" placeholder="Category" required />
+
+            <TransactionType>
+              <TransactionTypeButton value="income" variant="income">
+                <ArrowCircleUp size={24}></ArrowCircleUp>
+              </TransactionTypeButton>
+
+              <TransactionTypeButton value="outcome" variant="outcome">
+                <ArrowCircleDown size={24}></ArrowCircleDown>
+              </TransactionTypeButton>
+            </TransactionType>
 
             <button type="submit">Add</button>
           </form>
