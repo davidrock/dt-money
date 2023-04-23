0792e75dd7f4a65139ea555525cddae54b75ce2b
David Rocha <david.martinelli@farfetch.com>
2023-04-23 19:28:09 +0100

    create project and first components

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,39 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransacationsContainer,
+  TransactionsTable,
+} from "./styles";
+
+export function Transactions() {
+  return (
+    <>
+      <Header />
+      <Summary />
+
+      <TransacationsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Website Freelance</td>
+              <td>
+                <PriceHighlight variant="income">U$ 2500</PriceHighlight>
+              </td>
+              <td>Income</td>
+              <td>13 Apr 22</td>
+            </tr>
+            <tr>
+              <td>Groceries</td>
+              <td>
+                <PriceHighlight variant="outcome">- U$ 500</PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>13 Apr 22</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransacationsContainer>
+    </>
+  );
+}
